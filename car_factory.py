40ca6a03dcb4a0f062816b3fec8152b383867b75
Yue He <yuehe.neu@gmail.com>
2023-06-21 22:50:56 -0700

    assign new functionality

--- car_factory.py
+++ car_factory.py
@@ -4,34 +4,42 @@ from engine.willoughby_engine import WilloughbyEngine
 from engine.sternman_engine import SternmanEngine
 from battery.spindler_battery import SpindlerBattery
 from battery.nubbin_battery import NubbinBattery
+from tire.carrigan_tire import CarriganTire
+from tire.octoprime_tire import OctoprimeTire
 
 class CarFactory:
   @staticmethod
   def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage, sensor_result):
       engine = CapuletEngine(current_mileage, last_service_mileage)
       battery = NubbinBattery(current_date, last_service_date)
-      return Car(engine, battery)
+      tires = CarriganTire(sensor_result)
+      return Car(engine, battery, tires)
 
   @staticmethod
   def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage, sensor_result):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
-        return Car(engine, battery)
+        tires = OctoprimeTire(sensor_result)
+        return Car(engine, battery, tires)
   
   @staticmethod
   def create_palindrome(current_date, last_service_date, warning_light_on, sensor_result):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(current_date, last_service_date)
-        return Car(engine, battery)
+        tires = CarriganTire(sensor_result)
+        return Car(engine, battery, tires)
 
   @staticmethod
   def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage, sensor_result):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
-        return Car(engine, battery)
+        tires = OctoprimeTire(sensor_result)
+        return Car(engine, battery, tires)
 
   @staticmethod
   def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage, sensor_result):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
-        return Car(engine, battery)
+        tires = CarriganTire(sensor_result)
+        return Car(engine, battery, tires)
+        
